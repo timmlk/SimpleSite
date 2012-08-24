c2d8f68dd5b4eed6c22b5599051fdd07dac5897a
Timm Kristensen <timmkristensen@gmail.com>
2012-08-24 09:37:22 +0200

    First version

---
+++ app.js
@@ -0,0 +1,318 @@
+/**
+ * Module dependencies.
+ */
+
+var //connect = require('connect'),
+	jade = require('jade'), 
+	express = require('express'), 
+	routes = require('./routes'), 
+	http = require('http'), 
+	path = require('path'), 
+	models = require('./models.js'), 
+	mongoose = require('mongoose'), 
+	mongoStore = require('connect-mongodb'), 
+	db, 
+	Document, 
+	User, 
+	sys = require('util');
+
+var app = express();
+
+app.configure('development', function() {
+	console.log("configure dev");
+	app.set('db-uri', 'mongodb://localhost/webnode-dev');
+	app.use(express.errorHandler({
+		dumpExceptions : true
+	}));
+	app.set('view options', {
+		pretty : true
+	});
+});
+
+app.configure('test', function() {
+	app.set('db-uri', 'mongodb://localhost/webnode-test');
+	app.set('view options', {
+		pretty : true
+	});
+});
+
+app.configure('production', function() {
+	app.set('db-uri', 'mongodb://localhost/webnode-production');
+});
+
+app.configure(function() {
+	console.log("configure");
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.methodOverride());
+	// app.use(app.router);
+	app.use(express.methodOverride());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.session({
+		store : mongoStore(app.get('db-uri')),
+		secret : 'topsecret'
+	}));
+	app.use(express.logger({
+		format : '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms'
+	}))
+
+});
+
+// define models
+models.defineModels(mongoose, function() {
+	console.log("define models");
+	app.Document = Document = mongoose.model('Document');
+	appUser = User = mongoose.model('User');
+	// app.LoginToken = LoginToken = mongoose.model('LoginToken');
+	db = mongoose.connect(app.get('db-uri'));
+})
+function mongoStoreConnectionArgs() {
+	return {
+		dbname : db.db.databaseName,
+		host : db.db.serverConfig.host,
+		port : db.db.serverConfig.port,
+		username : db.uri.username,
+		password : db.uri.password
+	};
+}
+
+function loadUser(req, res, next) {
+	if (req.session.user_id) {
+		User.findById(req.session.user_id, function(err, user) {
+			if (user) {
+				req.currentUser = user;
+				next();
+			} else {
+				res.redirect('/sessions/new');
+			}
+		});
+	} else {
+		res.redirect('/sessions/new');
+	}
+}
+// setup route handlers
+//always try to set user
+app.all('*',function(req,res,next){
+	console.log("app.all : "+req+ ", "+res);
+	if(req.session.user_id){
+		User.findById(req.session.user_id, function(err, user) {
+			if (user) {
+				req.currentUser = user;
+				res.locals.user_email = user.email;
+			}
+		});	
+	}
+	next();
+});
+// default handler
+
+app.get('/', routes.index);
+
+// setup document handlers
+// List
+app.get('/documents.:format?', function(req, res) {
+	console.log("got req: " + req.url);
+	Document.find(function(err, documents) {
+
+		console.log(sys.inspect(documents));
+		if (!documents)
+			documents = [];
+		handleFormat(req, res, documents);
+	});
+});
+
+// Create
+app.post('/documents/:id.:format?', function(req, res) {
+	var document = new Document(req.body['document']);
+	document.save(function() {
+		handleFormat(req, res, document, null, function() {
+			res.redirect('/documents');
+		})
+	});
+});
+
+// Read
+app.get('/documents/:id.:format?/edit', function(req, res) {
+	console.log("got edit for id : " + req.params.id);
+	Document.findById(req.params.id, function(err, d) {
+		res.render('documents/edit.jade', {
+			d : d
+		});
+	});
+});
+// get for create form
+app.get('/documents/new', function(req, res) {
+	res.render('documents/new.jade', {
+		d : new Document()
+	});
+});
+
+// read for individual document
+app.get('/documents/:id.:format?', function(req, res) {
+	console.log("got read for id : " + req.params.id);
+	Document.findById(req.params.id, function(err, d) {
+		handleFormat(req, res, d, null, function() {
+			res.render('documents/edit.jade', { // todo
+				d : d
+			});
+		});
+	});
+});
+
+// Update
+app.put('/documents/:id.:format?', function(req, res) {
+	// Load the document
+	Document.findById(req.body.document.id, function(err, d) {
+		console.log("Update document: " + req.body.document.id);
+		// Do something with it
+		d.title = req.body.document.title;
+		d.data = req.body.document.data;
+		d.save(function() {
+			handleFormat(req, res, d, null, function() {
+				res.redirect('/documents');
+			});
+		})
+	});
+});
+
+// Delete
+app.del('/documents/:id.:format?',loadUser, function(req, res) {
+	console.log('deleting:' + req.params.id)
+	// Load the document
+	Document.findById(req.params.id, function(err, d) {
+		// remove the document
+		d.remove(function() {
+			// Respond according to the request format
+			handleFormat(req, res, d, null, function() {
+				res.redirect('/documents');
+			});
+
+		});
+	});
+
+});
+
+// Sessions
+app.get('/sessions/new', function(req, res) {
+	console.log("new session");
+	res.render('sessions/new.jade', {
+		user : new User()
+	});
+});
+
+app.post('/sessions', function(req, res) {
+	// Find the user and set the currentUser session variable
+	  User.findOne({ email: req.body.user.email }, function(err, user) {
+		    if (user && user.authenticate(req.body.user.password)) {
+		      req.session.user_id = user.id;
+
+		      // Remember me
+		      if (req.body.remember_me) {
+		        var loginToken = new LoginToken({ email: user.email });
+		        loginToken.save(function() {
+		          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+		          res.redirect('/documents');
+		        });
+		      } else {
+		        res.redirect('/documents');
+		      }
+		    } else {
+		      flash('error', 'Incorrect credentials',res);
+		      res.render('sessions/new', { user: new User()});
+		    }
+		  }); 
+});
+
+app.del('/sessions', loadUser, function(req, res) {
+	console.log("close session");
+	// Remove the session
+	if (req.session) {
+		req.session.destroy(function() {
+		});
+	}
+	res.redirect('/sessions/new');
+});
+
+app.get('/users/new', function(req, res) {
+	res.render('users/new', {
+		user : new User()
+	});
+});
+
+app.post('/users.:format?', function(req, res) {
+	console.log("new user");
+	var user = new User(req.body.user);
+
+	function userSaved() {
+		switch (req.params.format) {
+		case 'json':
+			res.send(user);
+			break;
+
+		default:
+			req.session.user_id = user.id;
+			res.redirect('/documents');
+		}
+	}
+
+	function userSaveFailed() {
+		res.render('users/new.jade', {
+			locals : {
+				user : user
+			}
+		});
+	}
+
+	user.save(userSaved, userSaveFailed);
+});
+
+function handleFormat(req, res, data/* json handler, default handler */) {
+	switch (req.params.format) {
+	case 'json':
+		if (arguments[3])
+			arguments[3]();
+		else {
+			if (data instanceof Array) {
+				res.send(data.map(function(d) {
+					console.log(d);
+					return d;
+				}));
+			} else {
+				res.send(data);
+			}
+		}
+		break;
+
+	default:
+		if (arguments[4]) {
+			arguments[4]();
+		} else {
+			res.render('documents/index.jade', {
+				documents : data
+			});
+		}
+	}
+}
+
+function flash(level, msg, res){
+	console.log("flash");
+	res.locals.msg = {
+			level : level,
+			message: msg
+	}
+}
+// finally create server
+http.createServer(app).listen(
+		app.get('port'),
+		function() {
+			console.log("Express server listening on port " + app.get('port'));
+			console.log('Express server listening on port %d, environment: %s',
+					app.get('port'), app.settings.env)
+			//console.log('Using connect %s, Express %s, Jade %s',
+			//		connect.version, express.version, jade.version);
+		});
---
+++ models.js
@@ -0,0 +1,84 @@
+var mongoose = require('mongoose').Mongoose, crypto = require("crypto");
+
+function defineModels(mongoose, fn) {
+	var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
+
+	/**
+	 * Model: Document
+	 */
+	Document = new Schema({
+		'title' : {
+			type : String,
+			index : true
+		},
+		'data' : String,
+		'tags' : [ String ],
+		'keywords' : [ String ],
+		'user_id' : ObjectId
+	});
+
+	Document.virtual('id').get(function() {
+		console.log("Doc.get" + this._id);
+		console.log("Doc.get" + this._id.toHexString());
+		return this._id.toHexString();
+	});
+
+	Document.pre('save', function(next) {
+		//this.keywords = extractKeywords(this.data);
+		next();
+	});
+
+	
+	/**
+	    * Model: User
+	    */
+	  function validatePresenceOf(value) {
+	    return value && value.length;
+	  }
+
+	  User = new Schema({
+	    'email': { type: String, validate: [validatePresenceOf, 'an email is required'], index: { unique: true } },
+	    'hashed_password': String,
+	    'salt': String
+	  });
+
+	  User.virtual('id')
+	    .get(function() {
+	      return this._id.toHexString();
+	    });
+
+	  User.virtual('password')
+	    .set(function(password) {
+	      this._password = password;
+	      this.salt = this.makeSalt();
+	      this.hashed_password = this.encryptPassword(password);
+	    })
+	    .get(function() { return this._password; });
+
+	  User.method('authenticate', function(plainText) {
+	    return this.encryptPassword(plainText) === this.hashed_password;
+	  });
+	  
+	  User.method('makeSalt', function() {
+	    return Math.round((new Date().valueOf() * Math.random())) + '';
+	  });
+
+	  User.method('encryptPassword', function(password) {
+	    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+	  });
+
+	  User.pre('save', function(next) {
+	    if (!validatePresenceOf(this.password)) {
+	      next(new Error('Invalid password'));
+	    } else {
+	      next();
+	    }
+	  });
+
+	mongoose.model('Document', Document);
+	mongoose.model('User', User);
+
+	fn();
+}
+
+exports.defineModels = defineModels;
